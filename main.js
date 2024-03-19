7cbd508ac2321c9ed13eda86e0980f60d9e75f3c
Taewon_KIM <gavy1124@naver.com>
2024-03-19 20:18:50 +0900

    v2

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-
-
+// 1. 랜덤숫자 만들기
+// 2. go버튼 동작
+// 2-1 . 결과 나오기 ( up, down, 정답)
+// reset버튼 동작
 
 
 
@@ -7,7 +9,7 @@ let randomNum = 0;
 let userInput = document.getElementById("userInput")
 let goBtn = document.getElementById("goBtn")
 let result = document.getElementById("result")
-
+let resetBtn = document.getElementById("resetBtn")
 
 
 
@@ -15,20 +17,31 @@ let result = document.getElementById("result")
 //ㅇ
 
 //버튼 동작
-goBtn.addEventListener("click",go)
+goBtn.addEventListener("click", go)
+resetBtn.addEventListener("click", reset)
 
 
 
-//go버튼
-function go(){
+
+
+//reset버튼 함수
+function reset(){
+    random()
+    result.textContent = "";
+}
+
+
+
+//go버튼 함수
+function go() {
     let userNum = userInput.value
     //체크 console.log(userNum)
 
-    if(userNum > randomNum){
+    if (userNum > randomNum) {
         result.textContent = "Down~~~"
-    }else if(userNum < randomNum){
+    } else if (userNum < randomNum) {
         result.textContent = "Up !!"
-    }else if(userNum == randomNum){
+    } else if (userNum == randomNum) {
         result.textContent = "♨ 정답 ♨"
     }
 }
